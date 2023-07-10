de1b4b714314c1e92b0a1fd2af4ed7dad05b4843
vaibhavps05 <vaibhavps3005@gmail.com>
2023-07-10 22:21:04 +0530

    multiPage update

--- app.js
+++ app.js
@@ -16,12 +16,42 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req,res){
+  res.render("home", {
+    homeStartingContentejs: homeStartingContent
+  });
+});
+
+
+app.get("/about", function(req,res){
+  res.render("about", {
+    aboutStartingContentejs: aboutContent
+  });
+});
+
+
+app.get("/contact", function(req,res){
+  res.render("contact", {
+    contactStartingContentejs: contactContent
+  });
+});
+
+
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
+
+
+app.post("/compose", function(req,res){
 
 
 
+  var post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  }
 
-
-
+})
 
 
 
