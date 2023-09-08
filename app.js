850d38c41584205ca3bcba19b95010748bd99379
vaibhavps05 <vaibhavps3005@gmail.com>
2023-09-08 21:31:09 +0530

    init completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const app = express();
 const dash = lodash();
 
@@ -62,26 +63,26 @@ app.post("/compose", function(req,res){
 
 app.get("/post/:topic", function(req,res){
 
-  const requestedTitle = req.params.topic;
-
-  dash.lowerCase(requestedTitle);
+  const requestedTitle = dash.lowerCase(req.params.topic);
 
 
   posts.forEach(function(post){
-    const storedTitle = post.title;
 
-    dash.lowerCase(storedTitle);
+    const storedTitle = dash.lowerCase(post.title);
 
-    if (storedTitle == requestedTitle) {
-      console.log("Match found!");
-    } else {
-      console.log("Incorrect!");
-    }
+    if (storedTitle === requestedTitle) {
+      res.render("post",{
+        title: post.title,
+        content: post.body
+      });
+    } 
 
 
   });
 
 });
+
+ 
 
 
 
